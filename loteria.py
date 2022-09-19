f3a651d1ed2beec20bb20be47361ef1d0c51dd18
draggons202 <94683849+draggons202@users.noreply.github.com>
2022-09-19 13:38:51 +0200

    Add files via upload

---
+++ loteria.py
@@ -0,0 +1,41 @@
+#veta = input ('Napíš nejaku vetu')
+#dlzka = len(veta)
+#print('Počet znakov v tvojej vete je:', dlzka)ň
+
+#retazec = 'python'
+#print(retazec[0])
+#print(retazec[4])
+
+#retazec = 'Python'
+#for i in range(len(retazec)):
+#    print(retazec[i])
+#    print(i)
+
+#veta1 = 'Programuje v Pythone'
+#veta2 = ''
+#for i in range(len(veta1)):
+#    if veta1[i] != ' ':
+#        veta2 = veta2+veta1[i]
+
+#print(veta1)
+#print(veta2)
+
+#veta1 = 'Programujeme v pythone'
+#veta2 = ''
+#for i in range (len(veta1)):
+#    if veta1[i] == 'o':
+#        veta2 = veta2+'*'
+#    else:
+#        veta2 = veta2+veta1[i]
+
+#print(veta1)
+#print(veta2)
+
+veta1 = 'Programujeme v Pythone'
+veta2 = ''
+for i in range (len(veta1)):
+    if veta1[1] != ' ':
+        veta2 = veta2+veta1[i]
+
+print(veta1)
+print(veta2)
